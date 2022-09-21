ea860060d0f94e8fe0ee1c87f0eafc0ce3930bab
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-09-21 16:29:40 -0400

    feat(): creating index file and connecting with mongoose

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app =express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+.connect(
+    process.env.MONGO_URL
+
+    )
+    .then(()=>console.log("Db connection sucssessful"))
+    .catch((errer) =>{console.log(errer)});
+    
+    // create our end point
+app.get("/api/test", () =>{
+    console.log("test is successful");
+});
+
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log("Backend server is running")
+});
