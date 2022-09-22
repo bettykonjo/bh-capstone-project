095022b58173f03870f5a6716618afb9d862a635
bettykonjo <84689462+bettykonjo@users.noreply.github.com>
2022-09-22 11:14:51 -0400

    feat():addind router

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app =express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -14,9 +15,12 @@ mongoose
     .catch((errer) =>{console.log(errer)});
     
     // create our end point
-app.get("/api/test", () =>{
-    console.log("test is successful");
-});
+    app.use(express.json());
+    app.use("/api/users", userRoute);
+
+// app.get("/api/test", () =>{
+//     console.log("test is successful");
+// });
 
 
 app.listen(process.env.PORT || 4000, () => {
